ae4f536c7847ed79a1a96598a22a4bb92ed88e19
Tom Jackson <tom.s.jackson090@gmail.com>
2022-01-28 18:44:20 +0000

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7063d0e17bc51ad65d40c370c96bc0be'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c3f4e3df-8d81-4bec-88f8-3688e530684f'
+};
