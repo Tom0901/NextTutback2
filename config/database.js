043e19fd2d961caa9f4721ae95fda98fbf8b2358
Tom Jackson <tom.s.jackson090@gmail.com>
2022-01-29 14:37:27 +0000

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,38 +1,14 @@
 const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => {
-  
-  if(env('NODE_ENV') === 'production') {
-    const config = parse(process.env.DATABASE_URL)
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'postgres',
-            host: config.host,
-            port: config.port,
-            database: config.database,
-            username: config.user,
-            password: config.password
-          }, 
-          options: {
-            ssl: false
-          }
-        }
-      }
-    }
-  }
-
-  return {
+module.exports = ({ env }) => {(
+   {
     defaultConnection: 'default',
     connections: {
       default: {
         connector: 'bookshelf',
         settings: {
           client: 'sqlite',
-          filename: env('DATABASE_FILENAME'),
+          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
         },
         options: {
           useNullAsDefault: true,
@@ -40,4 +16,4 @@ module.exports = ({ env }) => {
       },
     },
   }
-};
+)};
