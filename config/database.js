26e213e681d4eaf0ee3811b5171d7168cd2b6262
Tom Jackson <tom.s.jackson090@gmail.com>
2022-01-29 14:57:17 +0000

    revert db.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,31 @@
 const parse = require('pg-connection-string').parse;
 
-module.exports = ({ env }) => {(
-   {
+module.exports = ({ env }) => {
+  
+  if(env('NODE_ENV') === 'production') {
+    const config = parse(process.env.DATABASE_URL)
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'postgres',
+            host: config.host,
+            port: config.port,
+            database: config.database,
+            username: config.user,
+            password: config.password
+          }, 
+          options: {
+            ssl: false
+          }
+        }
+      }
+    }
+  }
+
+  return {
     defaultConnection: 'default',
     connections: {
       default: {
@@ -16,4 +40,4 @@ module.exports = ({ env }) => {(
       },
     },
   }
-)};
+};
